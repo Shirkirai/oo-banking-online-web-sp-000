15bd46706f0d6f8b2684954d1372132f32bb3e34
Jung Lee <lee.jung.s95@gmail.com>
2020-06-03 03:22:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       receiver.balance = receiver.balance + amount
       self.status = 'complete'
     else
-      "Transaction rejected. Please check your account balance"
+      "Transaction rejected. Please check your account balance."
     end
   end
 
