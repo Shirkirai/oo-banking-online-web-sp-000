c4e760f2d2a6521dea9ca46d89f76d6cc1f31ef7
Jung Lee <lee.jung.s95@gmail.com>
2020-06-03 03:21:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && sender.balance >= amount && status == 'open'
+    if valid? && sender.balance >= amount && self.status == 'open'
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
     end
