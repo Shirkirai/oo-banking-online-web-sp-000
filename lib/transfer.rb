e5d18f5553a87716dc71a44b21c31c7b524afc97
Jung Lee <lee.jung.s95@gmail.com>
2020-06-03 03:26:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,6 +34,9 @@ class Transfer
       sender.balance = sender.balance + amount
       receiver.balance = receiver.balance - amount
       self.status = 'reversed'
+
+    else
+      nil
     end
   end
 
