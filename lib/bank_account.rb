ece416676333a23318b0d1f4d839250b39a5d69a
Jung Lee <lee.jung.s95@gmail.com>
2020-06-03 02:38:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,11 +2,6 @@ class BankAccount
   attr_accessor :name, :balance, :status
 
   def initialize(name)
-    if name = "Avi"
-      @name = name
-    else
-      error
-    end
     @balance = 1000
     @status = 'open'
   end
